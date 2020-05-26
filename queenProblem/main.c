95aad7da983e64412a59f6221649787607677f58
saurav tanwar <tsaurav1711@likelion.org>
2020-05-26 17:18:21 +0900

    7queenproblem

--- queenProblem/main.c
+++ queenProblem/main.c
@@ -1,15 +1,90 @@
-//
-//  main.c
-//  queenProblem
-//
-//  Created by saurav tanwar on 5/26/20.
-//  Copyright © 2020 saurav tanwar. All rights reserved.
-//
 
+
+#define N 7
+#include <stdbool.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+void printSolution(int board[N][N])
+{
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            printf(" %d ", board[i][j]);
+        printf("\n");
+    }
+}
+
+
+bool isSafe(int board[N][N], int row, int col)
+{
+    int i, j;
+
+    
+    for (i = 0; i < col; i++)
+        if (board[row][i])
+            return false;
+
+   
+    for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+        if (board[i][j])
+            return false;
+
+    
+    for (i = row, j = col; j >= 0 && i < N; i++, j--)
+        if (board[i][j])
+            return false;
+
+    return true;
+}
+
+
+bool solveNQUtil(int board[N][N], int col)
+{
+   
+    if (col >= N)
+        return true;
+
+   
+    for (int i = 0; i < N; i++) {
+       
+        if (isSafe(board, i, col)) {
+           
+            board[i][col] = 1;
+
+         
+            if (solveNQUtil(board, col + 1))
+                return true;
+
+          
+            board[i][col] = 0;
+        }
+    }
+
+  
+    return false;
+}
+
+
+bool solveNQ()
+{
+    int board[N][N] = { { 0, 0, 0, 0 ,0 ,0 ,0},
+                        { 0, 0, 0, 0 ,0 ,0 ,0},
+                        { 0, 0, 0, 0 ,0 ,0 ,0},
+                        { 0, 0, 0, 0 ,0 ,0 ,0} };
+
+    if (solveNQUtil(board, 0) == false) {
+        printf("Solution does not exist");
+        return false;
+    }
+
+    printSolution(board);
+    return true;
+}
+
+
+int main()
+{
+    solveNQ();
     return 0;
 }
+
